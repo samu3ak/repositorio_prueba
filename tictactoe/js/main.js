413fadd3cb0130271145b27c7387462c2e8b0fd6
samu3ak <samuelbolivar2003@gmail.com>
2022-12-27 00:08:44 +0100

    tic tac toe finish

--- tictactoe/js/main.js
+++ tictactoe/js/main.js
@@ -2,27 +2,60 @@ $(document).ready(function () {
 
     var fields = document.querySelectorAll(".container div");
     var seleccionados = [];
+    var turnoX = true;
+    var partidaFinalizada = false;
 
     // User click any box
     $(".container div").click(function (e) {
         e.preventDefault();
         let seleccionado = $(this).attr("value");
 
-        if (seleccionados.indexOf(seleccionado) == -1) {
+        if (seleccionados.indexOf(seleccionado) == -1 && !partidaFinalizada) {
             seleccionados.push(seleccionado);
 
-
-            $(fields[seleccionado]).text("X");
-
-            // Generates random number in order to select a box that the user hasn't checked yet
-            if (seleccionados.length < 8) {
-                let random = Math.floor(Math.random() * 9);
-                while (seleccionados.indexOf(random.toString()) != -1) {
-                    random = Math.floor(Math.random() * 9);
-                }
-                $(fields[random]).text("O");
-                seleccionados.push(random.toString());
+            if (turnoX) {
+                $(fields[seleccionado]).text("X");
+                partidaFinalizada = haGanado("X");
+            } else {
+                $(fields[seleccionado]).text("O");
+                partidaFinalizada = haGanado("O");
+            }
+            turnoX = !turnoX;
+        }
+        if (partidaFinalizada) {
+            if (!turnoX) {
+                alert("Ha ganado X");
+            } else {
+                alert("Ha ganado O");
             }
         }
     });
+
+    // Checks if player won
+    function haGanado(turno) {
+        let one = 0, two = 1, three = 2;
+        let haGanado = false;
+        while (!haGanado && three <= 8) {
+            haGanado = $(`.container div[value=${one}]`).text() == turno
+                && $(`.container div[value=${two}]`).text() == turno
+                && $(`.container div[value=${three}]`).text() == turno;
+            one += 3, two += 3, three += 3;
+        }
+        one = 0, two = 3, three = 6;
+        while (!haGanado && three <= 8) {
+            haGanado = $(`.container div[value=${one}]`).text() == turno
+                && $(`.container div[value=${two}]`).text() == turno
+                && $(`.container div[value=${three}]`).text() == turno;
+            one += 1, two += 1, three += 1;
+        }
+        if (!haGanado) {
+            haGanado = ($(`.container div[value=0]`).text() == turno
+                && $(`.container div[value=4]`).text() == turno
+                && $(`.container div[value=8]`).text() == turno) ||
+                ($(`.container div[value=2]`).text() == turno
+                    && $(`.container div[value=4]`).text() == turno
+                    && $(`.container div[value=6]`).text() == turno);
+        }
+        return haGanado;
+    }
 });
